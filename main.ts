67e23fe9aa7c7d7d7ba5f3039f7bff3382936fdc
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-02-08 16:17:46 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 namespace particle{
 //% block
 //% block "spawn particle x $x y $y vx $vx vy $vy lifetime $lifetime image $image2 ax $ax ay $ay"
-//% image2.shadow=screen_image_picker
+//% image2=screen_image_picker
 export function spawnParticle (x: number, y: number, vx: number, vy: number, lifetime: number, image2: Image, ax: number, ay: number) {
     particles2.push([
     x,
