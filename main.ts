f0564dc55006ad18749f12a11c5bb64303c6ae59
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-02-08 16:00:48 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+
+//% color="#ff6e59"
+namespace particle{
+//% block
+//% image2=screen_image_picker
+export function spawnParticle (x: number, y: number, vx: number, vy: number, lifetime: number, image2: Image, ax: number, ay: number) {
+    particles2.push([
+    x,
+    y,
+    vx,
+    vy,
+    lifetime,
+    particles2.length,
+    ax,
+    ay
+    ])
+    particleImages.push(image2)
+}
+}
+spriteutils.createRenderable(9001, function (screen2) {
+    for (let value of particles2) {
+        spriteutils.drawTransparentImage(particleImages[value[5]], screen2, value[0], value[1])
+    }
+})
+let particleImages: Image[] = []
+let particles2: number[][] = []
+particles2 = []
+particleImages = []
+game.onUpdate(function () {
+    for (let value of particles2) {
+        value[2] = value[2] + value[6]
+        value[0] = value[0] + value[2]
+        value[3] = value[3] + value[7]
+        value[1] = value[1] + value[3]
+        value[4] = value[4] - 1
+        if (value[4] < 1) {
+            particles2.removeAt(particles2.indexOf(value))
+        }
+    }
+    if (particles2.length < 1) {
+        particleImages = []
+    }
+})
