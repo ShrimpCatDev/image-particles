d7b929c82391d5de44bd5abfae418c1b03c42494
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-02-08 18:02:09 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //% color="#ff7d59"
-
+/*hello*/
 namespace particles{
 //% block="add particle x $x y $y vx $vx vy $vy lifetime $lifetime image $image2 ax $ax ay $ay"
 //% image2.shadow=screen_image_picker
